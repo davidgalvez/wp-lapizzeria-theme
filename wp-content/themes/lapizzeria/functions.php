2a9f6b341d8efc0fbb30d09969a41d63c9926a1d
David Galvez <davidgalvezfis@gmail.com>
2022-07-11 06:42:42 -0500

    Agregando el bloque de widgets al theme

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -35,6 +35,20 @@ function lapizzeria_menus(){
 }
 add_action('init','lapizzeria_menus');
 
+/** Zona de Widgets para el theme */
+function lapizzeria_widgets(){
+    register_sidebar(array(
+        'name'=> 'Blog Sidebar',
+        'id'=> 'blog_sidebar',
+        'before_widget' => '<div class"widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
+
+add_action('widgets_init','lapizzeria_widgets');
+
 /* Agregar css class a botones de paginador*/
 function lapizzeria_paginate_links( $link ) {     
     return 'class="boton boton-secundario"'; 
