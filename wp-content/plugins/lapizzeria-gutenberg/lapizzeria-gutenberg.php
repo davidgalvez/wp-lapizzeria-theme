eea9d511048cc402f1e6d5e4def4171c8b848546
David Galvez <davidgalvezfis@gmail.com>
2022-07-16 06:12:36 -0500

    agregando archivo inicial del plugin para crear bloques gutenberg

---
+++ wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+    plugin Name: La Pizzeria Gutenberg blocks
+    PLugin URI:
+    Description: Agrega bloques de Gutenberg nativos para el theme Lapizzeria
+    Version: 1.0
+    Author: David Henry Galvez Valverde
+    Author URI: https://davidgalvezv.wordpress.com/
+    License: GPL2
+    License URI: https:www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if(!defined('ABSPATH')) exit;
