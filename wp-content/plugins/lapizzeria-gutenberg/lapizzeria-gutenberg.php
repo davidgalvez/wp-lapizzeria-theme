a0c11cffbd159b1f3603ab47779d1df61edf5cc0
David Galvez <davidgalvezfis@gmail.com>
2022-07-28 10:17:07 -0500

    Registrando el archivo principal para el editor de bloques

--- wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
+++ wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
@@ -11,3 +11,25 @@
 */
 
 if(!defined('ABSPATH')) exit;
+
+/** Registrar bloques, scripts y CSS */
+
+function lapizzeria_registrar_bloques()
+{
+    //Si gutemberg no existe, salir
+    if(function_exists('register_block_type'))
+    {
+        return;
+    }
+
+    // Registrar los bloques en el editor
+    wp_register_script
+    (
+        'lapizzeria-editor-script', //nombre unico
+        plugins_url('build/index.js',__FILE__), //archivo con los bloques
+        array('wp-blocks','wp-i18n','wp-element', 'wp-editor'), //dependencias
+        filemtime(plugin_dir_path(__FILE__).'build/index.js') //version
+    );
+    
+}
+add_action('init', 'lapizzeria_registrar_bloques');
