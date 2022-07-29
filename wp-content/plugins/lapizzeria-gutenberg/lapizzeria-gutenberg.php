559a8e19c1c9d292dd95be96f31fda7342f1257a
David Galvez <davidgalvezfis@gmail.com>
2022-07-29 10:00:18 -0500

    Creando categoría personalizada para nuestros bloques

--- wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
+++ wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
@@ -12,6 +12,22 @@
 
 if(!defined('ABSPATH')) exit;
 
+/** Categorías personalizadas */
+function lapizzeria_categoria_personalizada($categories, $post)
+{
+    return array_merge
+    (
+        $categories,
+        array
+        (
+            'slug' =>'lapizzeria',
+            'title'=> 'La Pizzeria',
+            'icon' => 'store'
+        )
+    );
+}
+add_filter('block_categories','lapizzeria_categoria_personalizada',10,2);
+
 /** Registrar bloques, scripts y CSS */
 
 function lapizzeria_registrar_bloques()
